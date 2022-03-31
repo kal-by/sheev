4996df68dbb20a9397affa3cc7bae98c9bbfa4d4
David Himelick <david.himelick@gmail.com>
2022-03-30 22:27:30 -0500

    Add more quotes

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const id = 'sheev'
 const advice = [
-  'DO IT',
-  'Did You Ever Hear The Tragedy Of Darth Plagueis The Wise?'
+  'Do it.',
+  'Did you ever hear the tragedy of Darth Plagueis the Wise?',
+  'Your feeble skills are no match for the power of the Dark Side.',
+  'Everything that has transpired has done so according to my design.',
+  'So be it...Jedi.',
+  'There is a great disturbance in the force.',
+  'You will find that it is you who are mistaken, about a great many things.',
+  'Execute Order Sixty-Six!',
+  'Power! Unlimited power!'
 ]
 
-const onClick = () => {
+const answer = () => {
   document.getElementById(id).innerHTML = advice[Math.floor(Math.random() * advice.length)]
 }
 
-window.addEventListener('load', onClick)
-document.body.addEventListener('click', onClick, true)
+window.addEventListener('load', answer)
+document.body.addEventListener('click', answer, true)
