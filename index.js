4f6cb015b09b327ad23c814a39ea0cd0dedef629
David Himelick <david.himelick@gmail.com>
2022-03-30 20:40:18 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const id = 'sheev'
+const advice = ['DO IT', 'Did You Ever Hear The Tragedy Of Darth Plagueis The Wise?']
+
+const onClick = () => {
+  const current = document.getElementById(id).innerHTML
+  let next = advice[Math.floor(Math.random() * advice.length)]
+  while (current === next) {
+    next = advice[Math.floor(Math.random() * advice.length)]
+  }
+  document.getElementById(id).innerHTML = next
+}
+
+document.body.addEventListener('click', onClick, true)
